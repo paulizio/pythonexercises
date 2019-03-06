32168dec6812289bd5631716d030a02820208ec3
paulizio <nordmap@tutanota.com>
2019-03-06 10:06:14 +0200

    TablePrinter ready

--- tableprinter.py
+++ tableprinter.py
@@ -4,5 +4,22 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
 
 
 def printTable(table):
+    #Store longest item in each list
     colWidths = [0] * len(tableData)
-    
+
+    #Find longest item in each list
+    for item in range(len(table[0])):
+        for column in range(len(table)):
+            if colWidths[column]<=len(table[column][item]):
+                colWidths[column]=len(table[column][item])
+                    
+
+    #Print out table with each column rjusted
+    for item in range(len(table[0])):
+        for column in range(len(table)):
+            print(table[column][item].rjust(colWidths[column]),end=" ")
+        print()
+
+
+printTable(tableData)
+
