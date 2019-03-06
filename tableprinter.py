c20348329d99efcbb8b1325a9395e441568f13d1
paulizio <nordmap@tutanota.com>
2019-03-06 09:42:54 +0200

    first

---
+++ tableprinter.py
@@ -0,0 +1,8 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+
+def printTable(table):
+    colWidths = [0] * len(tableData)
+    
